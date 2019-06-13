2f1a0c7735f720848b2918de7bb70a893f6deac9
fbma <fbma@cin.ufpe.br>
2019-06-13 19:25:03 -0300

    tentando ajustar, mas sem muito sucesso

--- checkbox.js
+++ checkbox.js
@@ -1,23 +1,22 @@
 
+bonsai.run(document.getElementById('tela'), {
+    
     width: 1000,
     height: 500,
     code: function() {
         
-        function add(cont){
-            document.getElementById('teste').textContent=cont;
-        }
         var myShape = new Rect(0, 0, 1000,500);
             myShape.fill('grey');
             myShape.addTo(stage);
             
             var cont =0;
-        stage.on('pointerdown',function(e){
+        stage.on('multi:pointerdown',function(e){
             point = new Circle(e.x,e.y,10);
             point.fill('red')
             .on('pointerdown', function(e) {
                 x = this.attr('x');
                 y = this.attr('y');
-                point.addTo(point.parent);
+                stage.addChild(point);
             })
             .on('multi:drag', function(e){
                 this.attr({
@@ -25,13 +24,14 @@
                     y: y + e.diffY
                 });
             })
-            .on('click', function(){
+            .on('pointerdown', function(){
                 stage.removeChild(point);
                 cont--;
             })
             stage.addChild(point);
             console.log(++cont);
-            add(cont);
         })
 
     }    
+  }
+);
